dddea3e92327fdb9e8661cebd9cb1c7cc91a8ae7
Alex Nielsen <alex.nielsen@gmail.com>
2019-04-02 10:11:13 -0400

    first 10 min of tut done, pick it back up at adding the db

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import {
-  getHTML,
-  getTwitterFollowers,
-  getInstagramFollowers
-} from "./lib/scraper";
+import express from "express";
+import { getTwitterCount, getInstagramCount } from "./lib/scraper";
 
-async function go() {
-  const iPromise = getHTML("https://instagram.com/therealalexnielsen");
-  const tPromise = getHTML("https://twitter.com/bigalshouse");
-  const [instagramHTML, twitterHTML] = await Promise.all([iPromise, tPromise]);
-  const instagramCount = await getInstagramFollowers(instagramHTML);
-  const twCount = await getTwitterFollowers(twitterHTML);
-  console.log(
-    `you have ${twCount} twitter followers and ${instagramCount} Instagram followers`
-  );
-}
-go();
+const app = express();
+app.get("/", (req, res) => res.send("try /scrape"));
+
+app.get("/scrape", async (req, res, next) => {
+  console.log("scraping!!!");
+  const [iCount, tCount] = await Promise.all([
+    getTwitterCount(),
+    getInstagramCount()
+  ]);
+  console.log(iCount, tCount);
+  res.json({ iCount, tCount });
+});
+
+app.listen(2093, () => console.log("example app running on port 2093"));
